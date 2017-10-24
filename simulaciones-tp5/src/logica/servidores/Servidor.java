5920aaacf25a4b3467cffd097fbe7c61945ce3f1
Nicolas Hefty <nicolas.hefty@dxc.com>
2017-10-24 00:40:56 -0300

    Servidores delegan al estado

--- simulaciones-tp5/src/logica/servidores/Servidor.java
+++ simulaciones-tp5/src/logica/servidores/Servidor.java
@@ -6,6 +6,8 @@
 package logica.servidores;
 
 import logica.servidores.colas.Cola;
+import logica.servidores.exceptions.NoAtendidos;
+import logica.servidores.exceptions.TieneQueCalibrar;
 import logica.servidores.state.EstadoServidor;
 
 /**
@@ -20,4 +22,45 @@ public interface Servidor
     void setCola(Cola c);
 
     String getNombre();
+    
+    default void apertura()
+    {
+        getEstado().apertura(this);
+    }
+    default void cierre() throws NoAtendidos
+    {
+        getEstado().cierre(this);
+    }
+    default void inicioAtencionRecepcion()
+    {
+        getEstado().inicioAtencionRecepcion(this);
+    }
+    default void finAtencionRecepcion()
+    {
+        getEstado().finAtencionRecepcion(this);
+    }
+    default void inicioCalibrado()
+    {
+        getEstado().inicioCalibrado(this);
+    }
+    default void finCalibrado()
+    {
+        getEstado().finCalibrado(this);
+    }
+    default void inicioDescarga()
+    {
+        getEstado().inicioDescarga(this);
+    }
+    default void finDescarga() throws TieneQueCalibrar
+    {
+        getEstado().finDescarga(this);
+    }
+    default void inicioPesaje()
+    {
+        getEstado().inicioPesaje(this);
+    }
+    default void finPesaje()
+    {
+        getEstado().finPesaje(this);
+    }
 }
